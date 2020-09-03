02d270964b485fad7a6cbfcea4ea51df55e8d6de
Centrix16 <tel1tsyn@yandex.ru>
2020-09-03 23:20:47 +0300

    add sml.c

---
+++ sml.h
@@ -0,0 +1,19 @@
+#ifndef __SIMPLE_MATRIX_LIBRARY_H_INCLUDED__
+#define __SIMPLE_MATRIX_LIBRARY_H_INCLUDED__
+
+typedef struct {
+	int *table;
+	int lines;
+	int columns;
+} SML_MATRIX;
+
+void matrix_init(SML_MATRIX *mat, int lines, int columns);
+void matrix_free(SML_MATRIX *mat);
+int matrix_get_pos(SML_MATRIX *mat, int line, int column);
+int matrix_get_size(SML_MATRIX *mat);
+int matrix_get(SML_MATRIX *mat, int line, int column);
+void matrix_set(SML_MATRIX *mat, int line, int column, int value);
+void matrix_print(SML_MATRIX *mat);
+void matrix_fill(SML_MATRIX *mat, int *array);
+
+#endif
