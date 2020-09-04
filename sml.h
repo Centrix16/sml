9934745799da9010f510c280ab4a0493dd283e0a
Centrix16 <tel1tsyn@yandex.ru>
2020-09-04 23:12:28 +0300

    added functions for addition, subtraction, return order and many functions _is_

--- sml.h
+++ sml.h
@@ -12,10 +12,20 @@ void matrix_free(SML_MATRIX *mat);
 void matrix_resize(SML_MATRIX *mat, int lines, int columns);
 int matrix_get_pos(SML_MATRIX *mat, int line, int column);
 int matrix_get_size(SML_MATRIX *mat);
+int matrix_get_order(SML_MATRIX *mat);
 int matrix_get(SML_MATRIX *mat, int line, int column);
 void matrix_set(SML_MATRIX *mat, int line, int column, int value);
 void matrix_print(SML_MATRIX *mat);
 void matrix_fill(SML_MATRIX *mat, int *array);
 bool matrix_is_equal(SML_MATRIX *mat1, SML_MATRIX *mat2);
+bool matrix_is_square(SML_MATRIX *mat);
+bool matrix_is_diagonal(SML_MATRIX *mat);
+bool matrix_is_identity(SML_MATRIX *mat);
+bool matrix_is_zero(SML_MATRIX *mat);
+bool matrix_is_column(SML_MATRIX *mat);
+bool matrix_is_line(SML_MATRIX *mat);
+void matrix_transp(SML_MATRIX *mat1, SML_MATRIX *mat2);
+bool matrix_add(SML_MATRIX *mat1, SML_MATRIX * mat2, SML_MATRIX *mat3);
+bool matrix_diff(SML_MATRIX *mat1, SML_MATRIX *mat2, SML_MATRIX *mat3);
 
 #endif
