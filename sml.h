bb9c0b19467e50e599af399de507ee2818d1e29f
Centrix16 <tel1tsyn@yandex.ru>
2020-09-04 17:28:31 +0300

    add functions matrix_resize() and matrix_is_equal()

--- sml.h
+++ sml.h
@@ -9,11 +9,13 @@ typedef struct {
 
 void matrix_init(SML_MATRIX *mat, int lines, int columns);
 void matrix_free(SML_MATRIX *mat);
+void matrix_resize(SML_MATRIX *mat, int lines, int columns);
 int matrix_get_pos(SML_MATRIX *mat, int line, int column);
 int matrix_get_size(SML_MATRIX *mat);
 int matrix_get(SML_MATRIX *mat, int line, int column);
 void matrix_set(SML_MATRIX *mat, int line, int column, int value);
 void matrix_print(SML_MATRIX *mat);
 void matrix_fill(SML_MATRIX *mat, int *array);
+bool matrix_is_equal(SML_MATRIX *mat1, SML_MATRIX *mat2);
 
 #endif
